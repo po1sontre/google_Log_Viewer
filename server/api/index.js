f85e758f9ce44fc178c81c46b077d9632379c7fc
Ghaith <ghaith.khadhri2809@gmail.com>
2025-05-07 10:11:30 +0200

    deleted unused files

--- server/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('./pubsub/auth');
-
-// PubSub authentication endpoint
-router.post('/pubsub/auth', auth);
-
-module.exports = router; 
